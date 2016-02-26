6f704c901c5f84dd63f56baed92e00cc24d133ef
Jimmy Gong <gong.jim@gmail.com>
2016-02-26 14:39:42 -0800

    Skeleton out

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var philotes = require('../lib');
+
+describe('Philotes', function() {
+  it('should have a test suite', function(done) {
+    done();
+  });
+
+  // Run remainder of specs
+  require('./api')();
+});
